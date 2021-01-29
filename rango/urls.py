aca1482aae2ab13ce1aa32521ab5bb57cdff940c
Morgan Bierey <bierey11@gmail.com>
2021-01-29 16:23:44 +0000

    excersise 3.6 commit before testing

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,7 @@ from rango import views
 app_name = 'rango'
 urlpatterns = [
 path('', views.index, name='index'),
+path('', views.about, name ='about'), #new line added excercise 3.6
 ]
 
 #this is the urls.py file which is mentioned  (to create) in section 3.5 of twd 
--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")#edited in excercise 3.6<>
+    
+    
+
+#below is the new method from excercise 3.6
+def about(request):
+    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>.")#edited in excercise 3.6 <>
