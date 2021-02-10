20bd8f77e0aa4ad1940670f1ed06e10a009b6e88
Morgan Bierey <bierey11@gmail.com>
2021-02-10 17:57:29 +0000

    chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
+#second from.. added in ex5.6
 # Register your models here.
+
+#class below created chapter 5 ex5
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category', 'url')
+
+admin.site.register(Category)
+#below changed chapter 5 ex , added PageAdmin
+admin.site.register(Page,PageAdmin)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+#exercise 5.3
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    #line below added in excercise at end of 5
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    #below class meta edited in ex 5.6
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
